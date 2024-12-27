29afbd22c25b2ac01d5762c4d7751aeba7fb2365
LXM <lexim0fff@gmail.com>
2024-12-28 00:40:13 +0100

    Update core.py

--- core.py
+++ core.py
@@ -1,129 +1,159 @@
+import logging
 import socket
 import time
 import os
-from threading import *
 import sqlite3
-from database.DataBase import DataBase
+import subprocess
+from threading import *
+
 from Logic.Device import Device
 from Logic.Player import Players
 from Logic.LogicMessageFactory import packets
 from Utils.Config import Config
-from Utils.Helpers import Helpers
-import json
+
+# logging.basicConfig(filename="errors.log", level=logging.INFO, filemode="w")
+
+subprocess.Popen(['python', 'autoshop.py'])
+
 def _(*args):
-	print('[INFO]', end=' ')
-	for arg in args:
-		print(arg, end=' ')
-	print()
-addr = {}
-block = []
+    print('[INFO]', end=' ')
+    for arg in args:
+        print(arg, end=' ')
+    print()
+
+
+def setup_database():
+    db_path = './database/Player/plr.db'
+    os.makedirs(os.path.dirname(db_path), exist_ok=True)
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS plrs (
+            token TEXT, 
+            lowID INT, 
+            name TEXT, 
+            trophies INT, 
+            gold INT, 
+            gems INT, 
+            starpoints INT, 
+            tickets INT, 
+            Troproad INT, 
+            profile_icon INT, 
+            name_color INT,
+            clubID INT, 
+            clubRole INT, 
+            brawlerData JSON, 
+            brawlerID INT, 
+            skinID INT, 
+            roomID INT, 
+            box INT, 
+            bigbox INT, 
+            online INT, 
+            vip INT, 
+            playerExp INT, 
+            friends JSON, 
+            SCC TEXT,
+            trioWINS INT, 
+            sdWINS INT, 
+            theme INT, 
+            quests JSON,
+            ban INT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+    print("[INFO] Database setup completed.")
+
+
 class Server:
-	Clients = {"ClientCounts": 0, "Clients": {}}
-	ThreadCount = 0
-	def __init__(self, ip: str, port: int):
-		self.server = socket.socket()
-		self.port = port
-		self.ip = ip
-	def start(self):
-		self.server.bind((self.ip, self.port))
-		print(f'Server | Lobby started! {self.ip}:{self.port}\nServer | Battle started! {self.ip}:6969')
-		plrsinfo = "database/Player/plr.db"
-		if os.path.exists(plrsinfo):
-			conn = sqlite3.connect("database/Player/plr.db")
-			c = conn.cursor()
-			c.execute("UPDATE plrs SET roomID=0")
-			c.execute("UPDATE plrs SET online=0")
-			c.execute("SELECT * FROM plrs")
-			conn.commit()
-			conn.close()
-		while True:
-			self.server.listen()
-			client, address = self.server.accept()
-			if address[0] in addr:
-				addr[address[0]] += 1
-			else:
-				addr[address[0]] = 0
-			if address[0] in block:
-				os.system(f"iptables -A INPUT -s {address[0]} -j DROP")
-				client.close()
-			elif addr[address[0]] >= 4:
-				os.system(f"iptables -A INPUT -s {address[0]} -j DROP")
-				block.append(address[0])
-				config = open('config.json', 'r')
-				content = config.read()
-				settings = json.loads(content)
-				settings['block'].append(address[0])
-				print(f"{settings['block']}")
-				client.close()
-			else:
-				ClientThread(client, address).start()
-				Server.ThreadCount += 1
+    Clients = {"ClientCounts": 0, "Clients": {}}
+    ThreadCount = 0
+
+    def __init__(self, ip: str, port: int):
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.port = port
+        self.ip = ip
+
+    def start(self):
+        if not os.path.exists('./config.json'):
+            print("Creating config.json...")
+            Config.create_config(self)
+
+        setup_database()
+
+        try:
+            self.server.bind((self.ip, self.port))
+        except OSError as e:
+            print(f"[ERROR] Unable to bind to {self.ip}:{self.port} - {e}")
+            return
+
+        _(f'Server started! Ip: {self.ip}, Port: {self.port}')
+        while True:
+            self.server.listen()
+            try:
+                client, address = self.server.accept()
+                _(f'New connection! Ip: {address[0]}')
+                ClientThread(client, address).start()
+                Server.ThreadCount += 1
+            except Exception as e:
+                print(f"[ERROR] Error accepting connection: {e}")
+
+
 class ClientThread(Thread):
-	def __init__(self, client, address):
-		super().__init__()
-		self.client = client
-		self.address = address
-		self.device = Device(self.client)
-		self.player = Players(self.device)
+    def __init__(self, client, address):
+        super().__init__()
+        self.client = client
+        self.address = address
+        self.device = Device(self.client)
+        self.player = Players(self.device)
 
-	def recvall(self, length: int):
-		data = b''
-		while len(data) < length:
-			s = self.client.recv(length)
-			if not s:
-				block.append(self.address[0])
-				break
-			data += s
-		return data
+    def recvall(self, length: int):
+        data = b''
+        while len(data) < length:
+            s = self.client.recv(length - len(data))
+            if not s:
+                print("Receive Error!")
+                break
+            data += s
+        return data
 
-	def run(self):
-		last_packet = time.time()
-		try:
-			while True:
-				header = self.client.recv(7)
-				if len(header) > 0:
-					last_packet = time.time()
-					packet_id = int.from_bytes(header[:2], 'big')
-					length = int.from_bytes(header[2:5], 'big')
-					data = self.recvall(length)
-					if packet_id in packets:
-						message = packets[packet_id](self.client, self.player, data)
-						message.decode()
-						message.process()
-						if packet_id == 10101:
-							Server.Clients["Clients"][str(self.player.low_id)] = {"SocketInfo": self.client}
-							Server.Clients["ClientCounts"] = Server.ThreadCount
-							self.player.ClientDict = Server.Clients
-				if time.time() - last_packet > 9:
-					addr[self.address[0]] = 0
-					del addr[self.address[0]]
-					DataBase.replaceValue(self, 'online', 0)
-					Server.ThreadCount -= 1
-					print(f"Player Online {Server.ThreadCount}")
-					self.client.close()
-					break
-		except ConnectionAbortedError:
-			addr[self.address[0]] = 0
-			del addr[self.address[0]]
-			DataBase.replaceValue(self, 'online', 0)
-			Server.ThreadCount -= 1
-			print(f"Player Online {Server.ThreadCount}")
-			self.client.close()
-		except ConnectionResetError:
-			addr[self.address[0]] = 0
-			del addr[self.address[0]]
-			DataBase.replaceValue(self, 'online', 0)
-			Server.ThreadCount -= 1
-			print(f"Player Online {Server.ThreadCount}")
-			self.client.close()
-		except TimeoutError:
-			addr[self.address[0]] = 0
-			del addr[self.address[0]]
-			DataBase.replaceValue(self, 'online', 0)
-			Server.ThreadCount -= 1
-			print(f"Player Online {Server.ThreadCount}")
-			self.client.close()
+    def run(self):
+        last_packet = time.time()
+        try:
+            while True:
+                header = self.client.recv(7)
+                if len(header) > 0:
+                    last_packet = time.time()
+                    packet_id = int.from_bytes(header[:2], 'big')
+                    length = int.from_bytes(header[2:5], 'big')
+                    data = self.recvall(length)
+
+                    if packet_id in packets:
+                        _(f'Received packet! Id: {packet_id}')
+                        message = packets[packet_id](self.client, self.player, data)
+                        message.decode()
+                        message.process()
+
+                        if packet_id == 10101:
+                            Server.Clients["Clients"][str(self.player.low_id)] = {"SocketInfo": self.client}
+                            Server.Clients["ClientCounts"] = Server.ThreadCount
+                            self.player.ClientDict = Server.Clients
+
+                    else:
+                        _(f'Packet not handled! Id: {packet_id}')
+
+                if time.time() - last_packet > 10:
+                    print(f"[INFO] Ip: {self.address[0]} disconnected!")
+                    self.client.close()
+                    break
+        except (ConnectionAbortedError, ConnectionResetError, TimeoutError):
+            print(f"[INFO] Ip: {self.address[0]} disconnected!")
+            self.client.close()
+
 
 if __name__ == '__main__':
-	server = Server('0.0.0.0', 9339)
-	server.start()
+    print("VBC28 REMAKE @lexim0ff")
+    server = Server('0.0.0.0', 9339)
+    server.start()
+
